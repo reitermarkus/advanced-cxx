0384e15505c893a835605d3e3a1aec3bb7475183
Markus Reiter <me@reitermark.us>
2020-10-12 01:52:23 +0200

    Refactor CMake configuration.

--- lit/cmake/CxxStandard.cmake
+++ lit/cmake/CxxStandard.cmake
@@ -1,3 +1,14 @@
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED True)
 set(CMAKE_CXX_EXTENSIONS False)
+set(CMAKE_EXPORT_COMPILE_COMMANDS True)
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  add_compile_options(-lstdc++fs)
+endif()
+
+add_compile_options(-pedantic)
+
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+  add_compile_options(-Wall -Werror)
+endif()
