717bb3fbe664f740c6b19fbf16115d7d354aada4
Markus Reiter <me@reitermark.us>
2020-10-12 02:05:33 +0200

    Require GCC 10 or higher.

--- lit/cmake/CxxStandard.cmake
+++ lit/cmake/CxxStandard.cmake
@@ -4,7 +4,9 @@ set(CMAKE_CXX_EXTENSIONS False)
 set(CMAKE_EXPORT_COMPILE_COMMANDS True)
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-  add_compile_options(-lstdc++fs)
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "10.0.0")
+    message(FATAL_ERROR "GCC 10 or higher is required")
+  endif()
 endif()
 
 add_compile_options(-pedantic)
