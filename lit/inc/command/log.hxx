b0112ae922f968e85c73b86281388f2f75873ca4
Markus Reiter <me@reitermark.us>
2020-10-23 21:58:12 +0200

    Improve `lit log` algorithm.

--- lit/inc/command/log.hxx
+++ lit/inc/command/log.hxx
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <functional>
+#include <memory>
 
 #include "command.hxx"
 #include "repository.hxx"
@@ -26,81 +27,144 @@ class Log: public Command {
 
     const auto latest_revision_n = latest_revision.number();
     const auto revisions = latest_revision_n + 1;
-    vector<vector<long long>> children(revisions, vector<long long>());
-    vector<size_t> unfinished(revisions, 0);
 
-    size_t indentation = 0;
-    size_t max_indentation = 0;
+    vector<shared_ptr<::lit::Commit>> commits(revisions, nullptr);
+    vector<vector<shared_ptr<::lit::Commit>>> children(revisions, vector<shared_ptr<::lit::Commit>>());
 
-    bool previous_was_merge_commit = false;
-
-    for (ssize_t i = latest_revision_n; i >= 0; i--) {
+    for (auto i = 0; i <= latest_revision_n; i++) {
       const auto commit = repo().commit(Revision(i));
 
       const auto parent_a = commit.parent_a();
       const auto parent_b = commit.parent_b();
 
+      commits[i] = shared_ptr<::lit::Commit>(new ::lit::Commit(commit));
+
       if (parent_a) {
-        children[parent_a.value().number()].push_back(i);
+        children[parent_a->number()].push_back(commits[i]);
       }
 
       if (parent_b) {
-        children[parent_b.value().number()].push_back(i);
+        children[parent_b->number()].push_back(commits[i]);
       }
+    }
 
-      const auto is_root_commit = !bool(parent_a);
-      const auto is_merge_commit = bool(parent_b);
+    vector<vector<size_t>> branches(revisions, vector<size_t>());
 
-      for (auto& child: children[i]) {
-        unfinished[child] -= 1;
+    for (auto i = 0; i <= latest_revision_n; i++) {
+      const auto commit = commits[i];
 
-        if (unfinished[child] == 0) {
-          indentation -= 1;
+      const auto parent_a = commit->parent_a();
+      const auto parent_b = commit->parent_b();
+
+      auto current_branch = 0;
+
+      if (i > 0) {
+        while (current_branch < branches[i - 1].size()) {
+          const auto parent = branches[i - 1][current_branch];
+
+          if ((parent_a && parent == parent_a->number()) || (parent_b && parent == parent_b->number())) {
+            cout << i << ": break at " << current_branch << " = " << parent << endl;
+            current_branch++;
+            break;
+          }
+
+          branches[i].push_back(parent);
+          current_branch++;
         }
       }
 
-      auto indentation_left = max_indentation;
+      branches[i].push_back(i);
 
-      for (size_t j = 0; j < indentation; j++) {
-        cout << "│ ";
-        indentation_left -= 1;
+      const auto children_count = children[i].size();
+      for (auto c = 0; (c + 1) < children_count; c++) {
+        auto child = children[i][c];
+
+        auto right_parent = child->parent_b();
+        if (right_parent && i == right_parent->number()) {
+          cout << commit->id() << " is right parent of " << child->id() << endl;
+          continue;
+        }
+
+        branches[i].push_back(i);
       }
 
-      cout << "◯";
+      if (i > 0) {
+        for (auto b = branches[i].size(); b < branches[i - 1].size(); b++) {
+          const auto parent = branches[i - 1][b];
 
-      if (is_root_commit) {
-        if (indentation_left) {
-          cout << "─┘";
+          branches[i].push_back(parent);
         }
-      } else if (is_merge_commit) {
-        if (indentation_left) {
-          cout << "─┤";
+      }
+    }
+
+    for (auto i = latest_revision_n; i <= latest_revision_n; i--) {
+      auto commit = commits[i];
+
+      auto parent_b = commit->parent_b();
+
+      bool printed_self = false;
+
+      for (auto b = 0; b < branches[i].size(); b++) {
+        auto r = branches[i][b];
+
+        if (r == i) {
+          if (printed_self) {
+            cout << (b == branches[i].size() - 1 ? "─┘" : "─┴");
+          } else {
+            if (b > 0) {
+              cout << " ";
+            }
+
+            cout << "◯";
+            printed_self = true;
+          }
         } else {
-          cout << "─┐";
-        }
+          bool is_right_parent = parent_b && parent_b->number() == r;
 
-      } else {
-        for (size_t j = 0; j < indentation_left; j++) {
-          cout << " │";
+          if (is_right_parent) {
+            cout << "─";
+
+            size_t right_parent_children = children[parent_b->number()].size();
+
+            switch (right_parent_children) {
+              case 1: {
+                cout << "┐";
+                break;
+              }
+              case 2: {
+                cout << "┤";
+                break;
+              }
+              default: {
+                cout << "┼";
+                break;
+              }
+            }
+
+          } else {
+            if (b > 0) {
+              cout << " ";
+            }
+
+            bool has_children = children[r].size() > 0;
+            if (has_children) {
+              cout << "│";
+            } else {
+              cout << " ";
+            }
+          }
         }
       }
 
       cout << " ";
-      if ((size_t)i == current_revision.number()) {
+
+      if (i == current_revision.number()) {
         cout << "← ";
       } else {
         cout << "  ";
       }
 
-      cout << commit.id() << " \"" << commit.message() << "\"" << endl;
-
-      if (!previous_was_merge_commit) {
-        unfinished[i] += is_merge_commit ? 2 : 1;
-        indentation += 1;
-        max_indentation = max(max_indentation, indentation);
-      }
-
-      previous_was_merge_commit = is_merge_commit;
+      cout << commit->id() << ": " << commit->message() << endl;
     }
 
     return 0;
